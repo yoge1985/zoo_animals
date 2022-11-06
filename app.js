0183d47b543beb4395e0eeffca5128fb70c2586c
unknown <jpgefen@gmail.com>
2022-11-06 22:39:17 +0000

    second commit

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let apiAnimals;
 const animalContainer = document.querySelector('.animal-container');
 const animalName = document.querySelector('.animal-name');
 const animalPic = document.getElementById('animal-pic');
+const animalLifespan = document.querySelector('.animal-lifespan');
+// const animalHabitat = doucment.querySelector('.animal-habitat');
 const nextAnimalBtn = document.querySelector('.next-animal');
 
-//event listener for next animal
+//event listener for next animal button
 nextAnimalBtn.addEventListener('click', getAnimal);
 
-// Get animal weight from API
+// Get animal from API
 async function getAnimal () {
     const apiUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand';
     try{
@@ -26,7 +28,10 @@ async function getAnimal () {
 //populates the info from the API request
 async function getAnimalInfo() {
     animalName.innerHTML = apiAnimals.name;
+    animalLifespan.innerHTML = 'LIFESPAN:' + ' ' + apiAnimals.lifespan;
+    // animalHabitat.innerHTML = 'HABITAT:' + ' ' + apiAnimals.habitat;
     animalPic.src = apiAnimals.image_link;
+
 }
 
 getAnimal();
